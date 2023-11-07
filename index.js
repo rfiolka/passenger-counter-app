8932a5771b055b24b586064bc5dccf398abaa268
rfiolka <roksana.fiolka@icloud.com>
2023-11-07 18:15:58 +0100

    reset counter to 0 afer saving

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0;
 
 function increment(){
-   count = count + 1
-    countEl.innerText = count
+   count += + 1
+    countEl.textContent = count
+
 }
 
-function save() {
-    console.log(count)
-}
+    function save() {
+
+        let countStr = count + " - "
+        saveEl.textContent += countStr
+        countEl.textContent = 0
+        count = 0
+    }
+
+    
